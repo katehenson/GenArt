d537b92eed8fc6d79544ff9714c4752173f498e4
Kate Henson <71353704+katehenson@users.noreply.github.com>
2020-09-25 13:04:31 -0700

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,7 +1,4 @@
-function sq(){
-t.fd(50);t.rt(90);t.fd(50);t.rt(90);
-t.fd(50);t.rt(90);t.fd(50);t.rt(90);
-}
+
 function hex(){
  t.fd(50);t.rt(60); t.fd(50);t.rt(60);t.fd(50);t.rt(60);
   t.fd(50);t.rt(60);t.fd(50);t.rt(60);t.fd(50);t.rt(60);
