3a3391a65eaaffee5c5e76d3b1f8e3f9a45455ab
Kate Henson <71353704+katehenson@users.noreply.github.com>
2020-09-25 13:07:49 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let t;
 function start(){
  t=new logo(300,100,0,"can1");
  t.c.lineWidth=1;
-t.c.strokeStyle="blue";
+t.c.strokeStyle="yellow";
 t.fd(100);t.rt(45);t.fd(100);t.rt(45);
-hex();
+sq();t.fd(100);hex();
 for(let j=0;j<10;j=j+1){
 	hex();t.rt(36);t.fd(j*5);
 }
