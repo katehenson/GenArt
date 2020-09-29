4a750f737f131416cf9f4e723b1de1aa8b909edf
Kate Henson <71353704+katehenson@users.noreply.github.com>
2020-09-29 12:51:38 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,8 @@ function start(){
  t=new logo(300,100,0,"can1");
  t.c.lineWidth=1;
 t.c.strokeStyle="blue";
-//sq();
-for(let j=0;j<12;j=j+1){
-t.fd(100);t.bk(100);
-t.rt(30);
-}
+
+like1();
 }
 
 function clear_Can(){
