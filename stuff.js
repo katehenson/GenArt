6b91bd5e447581d44d52370f29be1793f1c8045c
Kate Henson <71353704+katehenson@users.noreply.github.com>
2020-09-29 12:51:33 -0700

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,5 +1,18 @@
 
-function sq(){
-t.fd(50);t.rt(90);t.fd(50);t.rt(90);
-t.fd(50);t.rt(90);t.fd(50);t.rt(90);
+function sq(len){
+t.fd(len);t.rt(90);t.fd(len);t.rt(90);
+t.fd(len);t.rt(90);t.fd(len);t.rt(90);
+}
+function bunchSq(len){
+  for(let k=1;k<11;k=k+1){
+    sq(k*10);
+  }
+}
+
+
+function like1(){
+for(let j=1;j<13;j=j+1){
+//t.fd(j*10);t.bk(j*10);t.rt(30);
+t.fd(j*20);bunchSq(j*10);t.bk(j*20);t.rt(30);
+}  
 }
